a946949cadb2b1e8fb38d6766d3fb0918881bf11
Tony Tiratay <tony.tiratay@gmail.com>
2018-06-12 18:43:43 +0200

    Add mongo connexion

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose.connect(db)
+	.then(() => console.log('Connected to Mongo'))
+	.catch((err) => console.log(err));
 
 app.get('/', (req, res) => {
 	res.send('Hello World')
